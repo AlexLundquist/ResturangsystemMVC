41bdec7914e8eaaf7cf953f1bd4c8f31ae064e58
ChasAlex <alexander.lundquist@chasacademy.se>
2025-01-31 17:19:21 +0100

    test commit to main

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace ResturangsystemMVC
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            Console.WriteLine("Test push");
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
